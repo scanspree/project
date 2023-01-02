66df07e474a75af45b5d6df0d9652ace1cb500f1
Ashley_Fernandes <placidoashley9@gmail.com>
2023-01-02 19:36:11 +0530

    version4

--- Project/User/homepg.php
+++ Project/User/homepg.php
@@ -18,49 +18,42 @@
 
 <body>
     
-    <nav class="navbar vh-15">
+    <nav class="navbar fixed-top">
         <div class="container col-sm-1 justify-content-center">
             <a class="navbar-brand" id="logo" href="#">
-                <img src="Logo.png" alt="ScanSpree" width="500" height="120" class="d-inline-block align-text-top  ">
+                <img src="images/Logo.png" alt="ScanSpree" width="200" height="70" class="d-inline-block align-text-top  ">
     
             </a>
         </div>
         
 </nav>
     
-    <div class=" vh-25 container-fluid " >
-        <br><br><br>
-        <div class="row justify-content-center">
-
-            <div class="col-md-6 text-center" id="col1">
+    <div class="container-fluid fixed-bottom" >
+        <br>
+        <div class="row justify-content-center p-4 p-md-5">
+            <div class="col-md-12 col-sm-12 col-12 text-center" id="col1">
                 <h3>Welcome to <strong>ScanSpree</strong></h1>
                     <br>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipis
                         icing elit. Eligendi nisi porro aliquid minus
                         quidem ab aut eum iure earum debitis. Quae dic
-                        ta omnis sit molestias eos fugiat cumque, volu
-                        ptatibus officia assumenda voluptatem doloremqu
-                        e similique labore, ducimus nemo autem. Recusan
-                        dae eum esse ad optio, rem voluptatibus offici
-                        a sunt laboriosam tempore ab.
-                    </p>
+                        gfdfmhjgk,jbmncbvadaerdyhkjnmv bvcxfshfyjgjbnbccvzdgdfhgjkhmx
+                        </p>
             </div>
 
         </div>
 
-        <br>
         <div class="row">
-            <div class="col-3 "></div>
-            <div class="col-6 text-center " id="col1">
-                <a class="btn " id="btn" href="login.php" role="button"><b>Login</b></a>
-                <a class="btn " id="btn" href="reg.php" role="button"><b>Register</b></a>
+            <div class="text-center d-grid gap-2" id="col1">
+                <button class="btn " id="btn" href="login.php" role="button"><b>Login</b></button>
+                <button class="btn " id="btn" href="reg.php" role="button"><b>Register</b></button>
             </div>
-            <div class="col-3"></div>
         </div>
 
 
     </div>
+    
 </body>
 
 </html>
